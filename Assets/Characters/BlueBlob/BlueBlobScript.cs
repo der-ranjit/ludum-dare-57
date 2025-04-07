695dd778f69f9f42bb06f6d511e0a36b4f28da08
Lisa-Philine <lphiline@web.de>
2025-04-07 15:29:29 +0200

    fix a bit of blob animations

---
+++ Assets/Characters/BlueBlob/BlueBlobScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueBlobScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Jumping true if y velocity is greater than 0.1f
+        bool isJumping = GetComponent<Rigidbody>().velocity.y > 0.1f;
+        // GetComponentInChildren<Animator>().SetBool("isJumping", isJumping);
+
+        // TODO: death
+        bool isDead = false;
+        // GetComponentInChildren<Animator>().SetBool("hasDied", isDead);
+    }
+}
