b2a616c4e806504cfd2d4779638f20e7bfd232c9
marverati <marverati@gmail.com>
2025-04-08 03:26:53 +0200

    Title screeeeeen finally yay UI layer woopp wooop

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -3,14 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
-    [SerializeField] private string gameSceneName = "SampleScene";
     
     void Update()
     {
         // Check if any key was pressed
         if (Input.anyKeyDown)
         {
-            SceneManager.LoadScene(gameSceneName);
+            // Delete own game object
+            Destroy(gameObject);
         }
     }
 }
