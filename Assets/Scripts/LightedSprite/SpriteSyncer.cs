e139c5527ad0f0106a86c2c985769d1b7e04649e
Ranjit Mevius <ranjit.mevius@outlook.com>
2025-04-11 02:44:11 +0200

    fix sprite lighting by emulating 2 faces

---
+++ Assets/Scripts/LightedSprite/SpriteSyncer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SpriteSyncer : MonoBehaviour
+{
+    private SpriteRenderer original;
+    private SpriteRenderer clone;
+
+    void Start()
+    {
+        original = transform.parent.GetComponentInParent<SpriteRenderer>();
+        clone = GetComponent<SpriteRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        if (!original || !clone) { return; }
+
+        clone.sprite = original.sprite;
+        clone.flipX = original.flipX;
+        clone.flipY = original.flipY;
+        clone.color = original.color;
+    }
+}
